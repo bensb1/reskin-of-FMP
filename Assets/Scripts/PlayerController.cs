2fa6f19b628f1101a3f716545f4a5fce269a8644
bensb1 <56164275+bensb1@users.noreply.github.com>
2021-03-04 15:05:58 +0000

    player movement, jump, camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,7 +9,15 @@ public class PlayerController : MonoBehaviour
     {
          if (Input.GetKey(KeyCode.A))
         {
-            rb.velocity = new Vector2(-5, 0);
+            rb.velocity = new Vector2(-5, rb.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(5, rb.velocity.y);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 10f);
         }
     }
 }
