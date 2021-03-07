62d72c2ed03b786d5de9d6e4e78dcf969b2ffa4e
bensb1 <56164275+bensb1@users.noreply.github.com>
2021-03-07 17:05:00 +0000

    animator parameters,input,finite state machine

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,22 +4,59 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private enum State { idle,running,Jumping}
+    private State state = State.idle;
+
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
     private void Update()
     {
-         if (Input.GetKey(KeyCode.A))
+        float hDirection = Input.GetAxis("Horizontal");
+        if (hDirection < 0)
         {
             rb.velocity = new Vector2(-5, rb.velocity.y);
             transform.localScale =  new Vector2(-1, 1);
+            
         }
-        if (Input.GetKey(KeyCode.D))
-        {
+         else if (hDirection > 0)
+            {
             rb.velocity = new Vector2(5, rb.velocity.y);
             transform.localScale = new Vector2(1, 1);
+            
+            }
+         else
+        {
+            
         }
         if (Input.GetKeyDown(KeyCode.Space))
-        {
+            {
             rb.velocity = new Vector2(rb.velocity.x, 10f);
+            state = State.Jumping;
+            }
+        VelocityState();
+        anim.SetInteger("State", (int)state);
+    }
+    private void VelocityState()
+    { if(state == State.Jumping)
+        {
+
         }
+         else if(Mathf.Abs(rb.velocity.x) > 2f)
+        {
+            //Moving
+            state = State.running;
+        }
+    else
+        {
+            state = State.idle;
+        }
+    
     }
 }
