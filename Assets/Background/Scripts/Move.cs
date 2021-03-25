2671eecca254cb66f940f1ce86b6cd9bb8ade057
bensb1 <162532@chrome.escg.ac.uk>
2021-03-25 14:05:41 +0000

    started building my own level

--- Assets/Background/Scripts/Move.cs
+++ Assets/Background/Scripts/Move.cs
@@ -6,8 +6,7 @@ public class Move : MonoBehaviour
     float Target;
 	void Start()
 	{
-        print("Thanks for buying this, if you need any support, email support@dilapidatedmeow.com. " +
-            "Please note I cannot help with scripting related problems.");
+     
 	}
 
 	void Update()
