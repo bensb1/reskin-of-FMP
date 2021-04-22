89dfbaff36ce9f268ff0d83a810c4d3b047be2f0
bensb1 <162532@chrome.escg.ac.uk>
2021-04-22 11:53:26 +0100

    added enemy death

--- Assets/Scripts/swordmanEnemy.cs
+++ Assets/Scripts/swordmanEnemy.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class swordmanEnemy : MonoBehaviour
+public class swordmanEnemy : Enemy
 {
     public float speed;
     public float distance;
