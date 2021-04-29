a44add5132d2302a0523d84e2bb4bcc6c8f2ce63
bensb1 <162532@chrome.escg.ac.uk>
2021-04-29 16:07:19 +0100

    added in oxygen script

---
+++ Assets/Scripts/oxygenBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class oxygenBar : MonoBehaviour
+{
+    public Slider Oxygenbar;
+    private int maxOxygen = 100;
+    private int currentOxygen;
+    public static oxygenBar instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        currentOxygen = maxOxygen;
+        Oxygenbar.maxValue = maxOxygen;
+        Oxygenbar.value = maxOxygen;
+    }
+
+
+}
